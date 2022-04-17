1d6351c6369e494d662a22292084c0babbdfb94e
Monirul-98 <monirulsifat@gmail.com>
2022-04-17 11:35:57 +0600

    Banner added in home page

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,49 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./Banner.css";
+import banner1 from "../../../images/banner/banner-1.jpg";
+import banner2 from "../../../images/banner/banner-2.jpg";
+import banner3 from "../../../images/banner/banner-3.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner</h2>
-    </div>
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100 vh-100 image"
+          src={banner1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100 vh-100 image"
+          src={banner2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 vh-100 image"
+          src={banner3}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div>
       <h2>Featured Services</h2>
+      <div>{services.map()}</div>
     </div>
   );
 };
