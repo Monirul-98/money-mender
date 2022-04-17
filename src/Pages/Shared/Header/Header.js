b3ddd0d1cd883566d91fedcde799d6676e35f9a3
Monirul-98 <monirulsifat@gmail.com>
2022-04-17 11:20:42 +0600

    Nav bar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import CustomLink from "../../CustomLink/CustomLink";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
-    );
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Money Mender</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={CustomLink} to="/about">
+              Features
+            </Nav.Link>
+            <Nav.Link href="home#services">Services</Nav.Link>
+            <Nav.Link as={CustomLink} to="/blogs">
+              Blogs
+            </Nav.Link>
+            <Nav.Link as={CustomLink} to="/login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
